45527f8d8789fd5849e8a0299fdaa94fc40324f3
Zach Petch <zachpetch@gmail.com>
2020-05-18 22:11:08 -0700

    fancifying

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,214 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Drummerland</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
 
-        <title>iDrummer | Dammit!</title>
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Welcome to<br />
+						Drummerland</h1>
+						<p>A site where a drummer teaches other drummers about drums for <a href="https://www.urbandictionary.com/define.php?term=drummers">drummers</a>.</p>
+						<ul class="actions">
+							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+				<!-- Header -->
+					<header id="header">
+						<a href="/" class="logo">Drummerland</a>
+					</header>
 
-            .full-height {
-                height: 100vh;
-            }
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="/">Lessons</a></li>
+							<li><a href="/generic">Generic Page</a></li>
+							<li><a href="/elements">Elements</a></li>
+						</ul>
+						<ul class="icons">
+							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+				<!-- Main -->
+					<div id="main">
 
-            .position-ref {
-                position: relative;
-            }
+						<!-- Featured Post -->
+							<article class="post featured">
+								<header class="major">
+									<span class="date">Lesson 1</span>
+									<h2><a href="#">Terminology</a></h2>
+									<p>Learn the important difference between drums and bongos<br>(spoiler alert, no one likes people who call themselves bongists,<br>especially if you use a hard "g").</p>
+								</header>
+								<a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+								<ul class="actions special">
+									<li><a href="#" class="button large">View Lesson</a></li>
+								</ul>
+							</article>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										<span class="date">Lesson 2</span>
+										<h2><a href="#">Pair of Diddles</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+									<p>Paradiddles for dummies.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">View Lesson</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">Lesson 3</span>
+										<h2><a href="#">Tough Love</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+									<p>How does it feel to be one of the beautiful people?</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">View Lesson</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">Lesson 4</span>
+										<h2><a href="#">Tuning</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
+									<p>Make your floor tom sound like John Bonham (a dead British person, whose middle name was probably something like Harry or Henry or Herman or some shit).</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">View Lesson</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">Lesson 5</span>
+										<h2><a href="#">Latin</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">View Lesson</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">Lesson 6</span>
+										<h2><a href="#">Control</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
+									<p>You don't get to be the greatest drummer in the central Okanagan without busting some kneecaps.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">View Lesson</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">Lesson 7</span>
+										<h2><a href="#">Winning</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+									<p>I choose to refuse to confuse loose excuses for news with the true stews and juices of genuine gooses and substance abusing Canadian mooses.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">View Lesson</a></li>
+									</ul>
+								</article>
+							</section>
 
-            .content {
-                text-align: center;
-            }
+						<!-- Footer -->
+							<footer>
+								<div class="pagination">
+									<!--<a href="#" class="previous">Prev</a>-->
+									<a href="#" class="page active">1</a>
+									<a href="#" class="page">2</a>
+									<a href="#" class="page">3</a>
+									<span class="extra">&hellip;</span>
+									<a href="#" class="page">8</a>
+									<a href="#" class="page">9</a>
+									<a href="#" class="page">10</a>
+									<a href="#" class="next">Next</a>
+								</div>
+							</footer>
 
-            .title {
-                font-size: 84px;
-            }
+					</div>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+				<!-- Footer -->
+					<footer id="footer">
+						<section>
+							<form method="post" action="#">
+								<div class="fields">
+									<div class="field">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" />
+									</div>
+									<div class="field">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email" />
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="3"></textarea>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" /></li>
+								</ul>
+							</form>
+						</section>
+						<section class="split contact">
+							<section class="alt">
+								<h3>Address</h3>
+								<p>123 Cheap Incorporation St.<br />
+								Dover, Delaware 80242</p>
+							</section>
+							<section>
+								<h3>Phone</h3>
+								<p><a href="#">(250) 867-5309</a></p>
+							</section>
+							<section>
+								<h3>Email</h3>
+								<p><a href="#">justen@halfpackofbadgers.org</a></p>
+							</section>
+							<section>
+								<h3>Social</h3>
+								<ul class="icons alt">
+									<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; 2020</li></ul>
+					</div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+			</div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    iDrummer
-                </div>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
 
-                <div class="links">
-                    <a href="/lessons">Lessons</a>
-                    <a href="">Pricing</a>
-                    <a href="">About</a>
-                    <a href="">Blog</a>
-                    <a href="https://en.wikipedia.org/wiki/Fairy">Random</a>
-                </div>
-            </div>
-        </div>
-    </body>
+	</body>
 </html>
